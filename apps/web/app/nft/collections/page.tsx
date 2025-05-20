2efe2ef82c584aed12effce6083413ee2bed06ea
aii23 <a.ivlev.om@gmail.com>
2025-05-20 13:49:10 +0300

    Moved our collection to the top. Fixed problem with rocks

--- apps/web/app/nft/collections/page.tsx
+++ apps/web/app/nft/collections/page.tsx
@@ -1,12 +1,66 @@
 'use client';
 
-import { NFTCollectionIDList } from '../../../lib/types/nftTypes';
+import { ICollection, NFTCollectionIDList } from '../../../lib/types/nftTypes';
 import { api } from '../../../trpc/react';
 import CollectionCard from '../../../entities/CollectionCard';
 import NFTCollectionCarousel from '../../../sections/NFTCollectionCarousel';
+import { useEffect, useState } from 'react';
+
+const priorityCollections = [
+  {
+    name: 'zknoid',
+    weight: 10,
+  },
+  {
+    name: 'tileville',
+    weight: 7,
+  },
+  {
+    name: 'minaty',
+    weight: 9,
+  },
+  {
+    name: 'zeko',
+    weight: 8,
+  },
+];
 
 export default function CollectionsPage() {
   const { data: collections } = api.http.nft.getCollections.useQuery();
+
+  const [sortedCollections, setSortedCollections] = useState<ICollection[]>([]);
+  useEffect(() => {
+    if (collections) {
+      let topCollections = collections.filter(collection =>
+        priorityCollections.some(pc => {
+          console.log(
+            pc.name.toLocaleLowerCase(),
+            collection.name.toLocaleLowerCase(),
+            pc.name.toLocaleLowerCase() === collection.name.toLocaleLowerCase()
+          );
+          return pc.name.toLocaleLowerCase() === collection.name.toLocaleLowerCase();
+        })
+      );
+      let otherCollections = collections.filter(
+        collection =>
+          !priorityCollections.some(
+            pc => pc.name.toLocaleLowerCase() === collection.name.toLocaleLowerCase()
+          )
+      );
+      topCollections.sort((a, b) => {
+        const weightA =
+          priorityCollections.find(pc => pc.name.toLocaleLowerCase() === a.name.toLocaleLowerCase())
+            ?.weight || 0;
+        const weightB =
+          priorityCollections.find(pc => pc.name.toLocaleLowerCase() === b.name.toLocaleLowerCase())
+            ?.weight || 0;
+        return weightB - weightA;
+      });
+
+      console.log(topCollections, otherCollections);
+      setSortedCollections([...topCollections, ...otherCollections]);
+    }
+  }, [collections]);
 
   return (
     <div className={'w-full'}>
@@ -18,14 +72,13 @@ export default function CollectionsPage() {
       >
         <span className="text-[1.667vw] font-medium font-museo">NFT Collections</span>
         <div className="grid grid-cols-2 gap-[0.781vw]">
-          {collections &&
-            collections
-              // Sort Zknoid to the top
-              .sort((a, b) => {
-                if (a.name.toLowerCase() === NFTCollectionIDList.Zknoid) return -1;
-                if (b.name.toLowerCase() === NFTCollectionIDList.Zknoid) return 1;
-                return 0;
-              })
+          {sortedCollections &&
+            sortedCollections
+              // .filter(
+              //   (collection) =>
+              //     collection.name.toLocaleLowerCase() === NFTCollectionIDList.Zknoid ||
+              //     collection.name.toLocaleLowerCase() === NFTCollectionIDList.Tileville
+              // )
               .map((collection, index) => <CollectionCard key={index} collection={collection} />)}
         </div>
       </section>
