94d456999ebf37ede930d8d2160e4e38efc4f9c4
aii23 <a.ivlev.om@gmail.com>
2025-05-28 10:01:22 +0300

    Updated next.js config for next15 wasm

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,8 +1,8 @@
-import TerserPlugin from "terser-webpack-plugin";
-import path from "path";
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import {withSentryConfig} from '@sentry/nextjs';
+import TerserPlugin from 'terser-webpack-plugin';
+import path from 'path';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { withSentryConfig } from '@sentry/nextjs';
 // import ImageMinimizerPlugin from "image-minimizer-webpack-plugin";
 
 const __filename = fileURLToPath(import.meta.url);
@@ -13,8 +13,8 @@ const nextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: "https",
-        hostname: "**",
+        protocol: 'https',
+        hostname: '**',
       },
     ],
   },
@@ -22,23 +22,38 @@ const nextConfig = {
   async headers() {
     return [
       {
-        source: "/(.*)",
+        source: '/(.*)',
         headers: [
-          { key: "Cross-Origin-Opener-Policy", value: "same-origin" },
-          { key: "Cross-Origin-Embedder-Policy", value: "require-corp" },
+          { key: 'Cross-Origin-Opener-Policy', value: 'same-origin' },
+          { key: 'Cross-Origin-Embedder-Policy', value: 'require-corp' },
         ],
       },
     ];
   },
+
+  experimental: {
+    turbopack: false,
+  },
+
   webpack(config, { isServer }) {
     if (!isServer) {
       config.resolve.alias = {
         ...config.resolve.alias,
-        o1js: path.resolve(__dirname, "node_modules/o1js/dist/web/index.js"),
+        o1js: path.resolve(__dirname, 'node_modules/o1js/dist/web/index.js'),
       };
     }
 
-    config.experiments = { ...config.experiments, topLevelAwait: true };
+    config.experiments = {
+      ...config.experiments,
+      topLevelAwait: true,
+      asyncWebAssembly: true,
+    };
+
+    config.module.rules.push({
+      test: /\.wasm$/i,
+      type: 'webassembly/async',
+    });
+
     return {
       ...config,
       // module: {
@@ -103,79 +118,75 @@ const nextConfig = {
     };
   },
   eslint: {
-    dirs: ["app", "components", "constants", "containers", "games", "lib"],
+    dirs: ['app', 'components', 'constants', 'containers', 'games', 'lib'],
   },
   experimental: {
     reactCompiler: true,
-    optimizePackageImports: [
-      "@zknoid/sdk",
-      "@zknoid/games",
-      "zknoid-chain-dev",
-    ],
+    optimizePackageImports: ['@zknoid/sdk', '@zknoid/games', 'zknoid-chain-dev'],
   },
   productionBrowserSourceMaps: false,
-  transpilePackages: ["@zknoid/sdk", "@zknoid/games", "zknoid-chain-dev"],
+  transpilePackages: ['@zknoid/sdk', '@zknoid/games', 'zknoid-chain-dev'],
   images: {
     remotePatterns: [
       {
-        protocol: "https",
-        hostname: "res.cloudinary.com",
-        port: "",
-        pathname: "/dw4kivbv0/image/upload/**",
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        port: '',
+        pathname: '/dw4kivbv0/image/upload/**',
       },
       {
-        protocol: "https",
-        hostname: "gateway.pinata.cloud",
-        pathname: "/ipfs/**",
+        protocol: 'https',
+        hostname: 'gateway.pinata.cloud',
+        pathname: '/ipfs/**',
       },
       {
-        protocol: "https",
-        hostname: "picsum.photos",
-      }
+        protocol: 'https',
+        hostname: 'picsum.photos',
+      },
     ],
   },
 };
 
 export default withSentryConfig(nextConfig, {
-// For all available options, see:
-// https://github.com/getsentry/sentry-webpack-plugin#options
+  // For all available options, see:
+  // https://github.com/getsentry/sentry-webpack-plugin#options
 
-org: "zknoid-2z",
-project: "javascript-nextjs",
+  org: 'zknoid-2z',
+  project: 'javascript-nextjs',
 
-// Only print logs for uploading source maps in CI
-silent: !process.env.CI,
+  // Only print logs for uploading source maps in CI
+  silent: !process.env.CI,
 
-// For all available options, see:
-// https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
+  // For all available options, see:
+  // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
 
-// Upload a larger set of source maps for prettier stack traces (increases build time)
-widenClientFileUpload: true,
+  // Upload a larger set of source maps for prettier stack traces (increases build time)
+  widenClientFileUpload: true,
 
-// Automatically annotate React components to show their full name in breadcrumbs and session replay
-reactComponentAnnotation: {
-enabled: true,
-},
+  // Automatically annotate React components to show their full name in breadcrumbs and session replay
+  reactComponentAnnotation: {
+    enabled: true,
+  },
 
-// Route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
-// This can increase your server load as well as your hosting bill.
-// Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
-// side errors will fail.
-tunnelRoute: "/monitoring",
+  // Route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
+  // This can increase your server load as well as your hosting bill.
+  // Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
+  // side errors will fail.
+  tunnelRoute: '/monitoring',
 
-// Hides source maps from generated client bundles
-hideSourceMaps: true,
+  // Hides source maps from generated client bundles
+  hideSourceMaps: true,
 
-// Automatically tree-shake Sentry logger statements to reduce bundle size
-disableLogger: true,
+  // Automatically tree-shake Sentry logger statements to reduce bundle size
+  disableLogger: true,
 
-// Enables automatic instrumentation of Vercel Cron Monitors. (Does not yet work with App Router route handlers.)
-// See the following for more information:
-// https://docs.sentry.io/product/crons/
-// https://vercel.com/docs/cron-jobs
-automaticVercelMonitors: true,
-sourcemaps: {
-  disable: true,
-  deleteSourcemapsAfterUpload: true
-}
+  // Enables automatic instrumentation of Vercel Cron Monitors. (Does not yet work with App Router route handlers.)
+  // See the following for more information:
+  // https://docs.sentry.io/product/crons/
+  // https://vercel.com/docs/cron-jobs
+  automaticVercelMonitors: true,
+  sourcemaps: {
+    disable: true,
+    deleteSourcemapsAfterUpload: true,
+  },
 });
